8c676f2a491e8850bd63238b56503b4b998dd189
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-04-19 21:34:18 +0900

    feat: Gesture

--- app/src/main/java/io/github/mikan/sample/animation/AnimationSample.kt
+++ app/src/main/java/io/github/mikan/sample/animation/AnimationSample.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.ExperimentalTransitionApi
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.RepeatMode
@@ -38,6 +39,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -67,19 +69,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import io.github.mikan.sample.animation.ui.theme.AnimationTheme
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 /**
  * references
@@ -669,6 +676,33 @@ fun InfiniteTransitionSample(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun Gesture(modifier: Modifier = Modifier) {
+    val offset = remember { Animatable(Offset.Zero, Offset.VectorConverter) }
+    val scope = rememberCoroutineScope()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                // using high level api
+                detectTapGestures {
+                    scope.launch {
+                        offset.animateTo(it)
+                    }
+                }
+            }
+    ) {
+        Box(
+            modifier = Modifier
+                .offset { offset.value.toIntOffset() }
+                .size(100.dp)
+                .background(Color.Blue, shape = CircleShape)
+        )
+    }
+}
+
+private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
+
 // Preview
 @Preview
 @Composable
@@ -733,6 +767,16 @@ private fun ValueBasedAnimationSamplesPreview() {
                 AnimatingBox()
                 InfiniteTransitionSample()
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AdvancedAnimationSample() {
+    AnimationTheme {
+        Surface {
+            Gesture()
         }
     }
 }
